9523e05c2d5209c0acef2870533932cc170443b3
Rachel Moon <rachel.myoung.moon@gmail.com>
2022-01-23 19:28:27 -0500

    build: dockerized model api server

--- src/bee_detection_model/functions.py
+++ src/bee_detection_model/functions.py
@@ -1,131 +1,28 @@
-# For running inference on the TF-Hub module.
 import tensorflow as tf
-import tensorflow_hub as hub
-
-# For downloading the image.
-import matplotlib.pyplot as plt
-import tempfile
-from six.moves.urllib.request import urlopen
-from six import BytesIO
-
-# For drawing onto the image.
-import numpy as np
 from PIL import Image
-from PIL import ImageColor
-from PIL import ImageDraw
-from PIL import ImageFont
-from PIL import ImageOps
-
-# For measuring the inference time.
+from io import BytesIO
 import time
 
-# helper visualization functions 
-def display_image(image):
-    fig = plt.figure(figsize=(20, 15))
-    plt.grid(False)
-    plt.imshow(image)
 
 
-def download_and_resize_image(url, new_width=256, new_height=256,
-                              display=False):
-    _, filename = tempfile.mkstemp(suffix=".jpg")
-    response = urlopen(url)
-    image_data = response.read()
-    image_data = BytesIO(image_data)
-    pil_image = Image.open(image_data)
-    pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
-    pil_image_rgb = pil_image.convert("RGB")
-    pil_image_rgb.save(filename, format="JPEG", quality=90)
-    print("Image downloaded to %s." % filename)
-    if display:
-        display_image(pil_image)
-    return filename
 
-
-def draw_bounding_box_on_image(image,
-                               ymin,
-                               xmin,
-                               ymax,
-                               xmax,
-                               color,
-                               font,
-                               thickness=4,
-                               display_str_list=()):
-    """Adds a bounding box to an image."""
-    draw = ImageDraw.Draw(image)
-    im_width, im_height = image.size
-    (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
-                                ymin * im_height, ymax * im_height)
-    draw.line([(left, top), (left, bottom), (right, bottom), (right, top),
-             (left, top)],
-            width=thickness,
-            fill=color)
-
-    # If the total height of the display strings added to the top of the bounding
-    # box exceeds the top of the image, stack the strings below the bounding box
-    # instead of above.
-    display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
-    # Each display_str has a top and bottom margin of 0.05x.
-    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
-
-    if top > total_display_str_height:
-        text_bottom = top
-    else:
-        text_bottom = top + total_display_str_height
-    # Reverse list and print from bottom to top.
-    for display_str in display_str_list[::-1]:
-        text_width, text_height = font.getsize(display_str)
-        margin = np.ceil(0.05 * text_height)
-        draw.rectangle([(left, text_bottom - text_height - 2 * margin),
-                        (left + text_width, text_bottom)],
-                       fill=color)
-        draw.text((left + margin, text_bottom - text_height - margin),
-                  display_str,
-                  fill="black",
-                  font=font)
-        text_bottom -= text_height - 2 * margin
-
-
-def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
-    """Overlay labeled boxes on an image with formatted scores and label names."""
-    colors = list(ImageColor.colormap.values())
-
-    try:
-        font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSansNarrow-Regular.ttf",
-                              25)
-    except IOError:
-        print("Font not found, using default font.")
-        font = ImageFont.load_default()
-
-    for i in range(min(boxes.shape[0], max_boxes)):
-        if scores[i] >= min_score:
-            ymin, xmin, ymax, xmax = tuple(boxes[i])
-            display_str = "{}: {}%".format(class_names[i].decode("ascii"),
-                                         int(100 * scores[i]))
-            color = colors[hash(class_names[i]) % len(colors)]
-            image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
-            draw_bounding_box_on_image(
-              image_pil,
-              ymin,
-              xmin,
-              ymax,
-              xmax,
-              color,
-              font,
-              display_str_list=[display_str])
-            np.copyto(image, np.array(image_pil))
-    return image
+def preprocess_and_save_input_image(img, input_filename, max_size=(1028, 1028)):
+    image_raw = Image.open(BytesIO(img)).convert('RGB')
+    image_raw.thumbnail(max_size, Image.ANTIALIAS) # rescale image to be smaller than max size
+    image_raw.save(input_filename)
 
 
 
 
-# helper functions for model prediction
 def load_img(path):
     img = tf.io.read_file(path)
     img = tf.image.decode_jpeg(img, channels=3)
     return img
 
-def run_detector(detector, path):
+
+
+
+def run_detector(detector, path: str):
     img = load_img(path)
     converted_img  = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
     start_time = time.time()
@@ -137,25 +34,22 @@ def run_detector(detector, path):
     print("Found %d objects." % len(result["detection_scores"]))
     print("Inference time: ", end_time-start_time)
 
-    # image_with_boxes = draw_boxes(
-    #     img.numpy(), result["detection_boxes"],
-    #     result["detection_class_entities"], result["detection_scores"])
-
-    # display_image(image_with_boxes)
     return result
 
 
 
-def filter_bees(result, min_score=0.2):
+def filter_bees(result: dict, min_score: float = 0.2):
+
     print(f"Total {len(result['detection_boxes'])} objects detected.")
-    # filter by only bees or insect labeled objects
+    # filter by only bees or insect labels
     bee_idx = [idx for idx, ele in enumerate(result["detection_class_entities"]) if ele == b"Bee" or ele == b"Insect"]
     print(f"Among them, a total of {len(bee_idx)} are classified as bee or insect.")
+    
     # filter by high confidence score
     bee_idx_high_conf = [ele for ele in bee_idx if (result["detection_scores"][ele] >= min_score)]
     print(f"Among them, a total of {len(bee_idx_high_conf)} have prediction confidence score of at least {min_score}.")
     
-    # filter result object
+    # apply filter
     result_bees = {'detection_class_entities':[],
                   'detection_scores':[],
                   'detection_boxes':[]}
