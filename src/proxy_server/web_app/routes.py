2e79ac9e10f9115cbbb8ed1a7be272106470b22c
Rachel Moon <rachel.myoung.moon@gmail.com>
2022-01-29 20:26:53 -0500

    feat: frontend image and basic report functionality

--- src/proxy_server/web_app/routes.py
+++ src/proxy_server/web_app/routes.py
@@ -1,28 +1,79 @@
 from web_app import app
-from flask import request, render_template, url_for, redirect, json, jsonify
+from flask import request, render_template, redirect, json, url_for, session
 import requests, os
 import uuid
+import numpy as np
+from web_app.functions import report_image
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+
+    error_message = None
+    # display error message if exists
+    if session.get('error_message') != None:
+        error_message = session.get('error_message')
+        session.pop('error_message')
+    
+    return render_template("index.html", error_message = error_message)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 @app.route("/predict", methods = ['POST'])
 def make_prediction():
 
-    # get input image file
+    # load and save input image file
     f = request.files['input_image']
+    if f.filename == '':
+        session['error_message'] = 'Please select an image file.'
+        return redirect(url_for('index'))
+    
     filename = f"{f.filename.split('.')[0]}_{uuid.uuid4().hex}.{f.filename.split('.')[-1]}" 
     full_filename = f"web_app/static/img/uploads/{filename}" 
     f.save(full_filename)
 
-    # call bee detection model server
+    # call bee detection model server to get bee detection box results
     r = requests.post('http://192.168.1.10:8000/predict', files={'file': (full_filename, open(full_filename, 'rb'))})
     # os.remove(filename)
-    result = r.json() # returns dictionary
-    print(result)
-    # call bee health model server
-    # input: {'file': hive_image, 'result': filtered_bee}
-    # output: {'label': ['healthy', 'varroa beetle', ...], 'confidence': [0.3456, 0.5352, ...]}
+    obj_result = r.json() # returns dictionary
+    bees_count = len(obj_result['detection_scores'])
     
-    return render_template("report.html", input_image = filename, result = result)
+
+    # if bee detected, call bee health model server to get health predictions
+    if bees_count > 0:
+        # call bee health model server
+        # input: {'file': hive_image, 'result': filtered_bee}
+        # output: {'label': ['healthy', 'varroa beetle', ...], 'confidence': [0.3456, 0.5352, ...]}
+            # create dummy health output json
+        health_labels = ["healthy", "varroa beetles", "ant problems", "hive being robbed", "missing queen"]
+        health_result = {
+            'label': np.random.choice(health_labels, size = len(obj_result['detection_scores']), p=[0.7, 0.1, 0.1, 0.05, 0.05]).tolist(),
+            'confidence': np.random.uniform(size=len(obj_result['detection_scores'])).tolist()
+        }
+    else:
+        health_result = {'label': [],
+                        'confidence': []}
+
+
+    # create visualization report
+    dest_fp = report_image(full_filename, obj_result, health_result)
+    
+    counts = {
+        'bees': bees_count,
+        'healthy': health_result['label'].count("healthy"),
+        'varroa beetles': health_result['label'].count("varroa beetles"),
+        'ant problems': health_result['label'].count("ant problems"),
+        'hive being robbed': health_result['label'].count("hive being robbed"),
+        'missing queen': health_result['label'].count("missing queen"),
+    }
+
+    print(obj_result)
+    print('')
+    print(health_result)
+    print('')
+    print(counts)
+
+
+    return render_template("report.html", input_image = dest_fp.split('/')[-1], result = health_result, counts = counts)
